865d908056ba3908fcf85441eb3cc4a7d84ad5f8
Vitoan <119367979+Vitoan@users.noreply.github.com>
2026-05-19 20:54:02 -0300

    rolesfix

--- src/middlewares/roles.js
+++ src/middlewares/roles.js
@@ -1,1 +1,21 @@
-roles.js
+src\middlewares\roles.js
+@@ -1,19 +1 @@
+const verificarRol = (rolesPermitidos) => {
+    return (req, res, next) => {
+        if (!req.session.usuario) {
+            return res.redirect('/auth/login');
+        }
+
+        if (rolesPermitidos.includes(req.session.usuario.rol)) {
+            next();
+        } else {
+            // Renderizar página de error 403 o redirigir
+            res.render('error', { 
+                mensaje: 'Acceso Denegado: No tienes permisos para realizar esta acción.',
+                usuario: req.session.usuario
+            });
+        }
+    }
+};
+
+module.exports = verificarRol;
