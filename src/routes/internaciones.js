de078b9acaa74b5ba22d788c28698d5609e4e1cd
Vitoan <119367979+Vitoan@users.noreply.github.com>
2026-07-02 14:16:11 -0300

    fix alta

--- src/routes/internaciones.js
+++ src/routes/internaciones.js
@@ -2,17 +2,19 @@ const express = require('express');
 const router = express.Router();
 const internacionController = require('../controllers/internacionController'); 
 
+const checkRole = require('../middlewares/roles');
+
 // 1. Mostrar el formulario
 router.get('/nuevo', internacionController.renderCreate);
 
 // 2. Guardar los datos en la base de datos
 router.post('/nuevo', internacionController.create);
 
-// 3. Mostrar formulario de Alta
-router.get('/:id/alta', internacionController.mostrarFormularioAlta);
+// 3. Mostrar formulario de Alta (Solo Médicos y Admin)
+router.get('/:id/alta', checkRole(['Admin', 'Medico']), internacionController.mostrarFormularioAlta);
 
-// 4. Procesar el Alta Médica
-router.post('/:id/alta', internacionController.darAlta);
+// 4. Procesar el Alta Médica (Solo Médicos y Admin)
+router.post('/:id/alta', checkRole(['Admin', 'Medico']), internacionController.darAlta);
 
 // 5. Transferencia de Cama
 router.get('/:id/transferir', internacionController.mostrarFormularioTransferencia);
